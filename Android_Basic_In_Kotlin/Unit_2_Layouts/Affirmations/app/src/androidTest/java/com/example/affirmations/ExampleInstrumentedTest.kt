277dc41fdfaeb51cef865c5963e800afc2061563
Rui Wu <ruiwu@Ruis-MacBook-Air.local>
2022-04-04 21:41:17 -0400

    update adapter test

--- Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
+++ Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -1,5 +1,13 @@
 package com.example.affirmations
 
+import android.support.test.espresso.contrib.RecyclerViewActions
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,22 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.affirmations", appContext.packageName)
-    }
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+//    @Test
+//    fun scrollToPosition(){
+//        onView(withId(R.id.recycler_view)).perform(
+//            RecyclerViewActions
+//                .scrollToPosition<RecyclerView.ViewHolder>(9))
+//        )
+//
+//        Espresso.onView(ViewMatchers.withText(R.string.affirmation10))
+//            .check(
+//                ViewAssertions.matches(ViewMatchers.isDisplayed())
+//            )
+//
+//    }
+
 }
--- Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/test/java/com/example/affirmations/ExampleUnitTest.kt
+++ Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/test/java/com/example/affirmations/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.example.affirmations
 
+import android.content.Context
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.model.Affirmation
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val context = mock(Context::class.java)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun adapter_size(){
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+
+        val adapter = ItemAdapter(context, data)
+
+        fun getItemCount() = data.size
+
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+
+
+
     }
 }
