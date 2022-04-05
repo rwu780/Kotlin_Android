3453080fa7a6155bfc78a7e7c2e9508003bb3450
Rui Wu <ruiwu@Ruis-MacBook-Air.local>
2022-04-05 08:21:15 -0400

    add instrumented test

--- Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
+++ Android_Basic_In_Kotlin/Unit_2_Layouts/Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -1,20 +1,19 @@
 package com.example.affirmations
 
-import android.support.test.espresso.contrib.RecyclerViewActions
+
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+
 import org.junit.Rule
 
 /**
@@ -28,18 +27,18 @@ class ExampleInstrumentedTest {
     @get:Rule
     val activity = ActivityScenarioRule(MainActivity::class.java)
 
-//    @Test
-//    fun scrollToPosition(){
-//        onView(withId(R.id.recycler_view)).perform(
-//            RecyclerViewActions
-//                .scrollToPosition<RecyclerView.ViewHolder>(9))
-//        )
-//
-//        Espresso.onView(ViewMatchers.withText(R.string.affirmation10))
-//            .check(
-//                ViewAssertions.matches(ViewMatchers.isDisplayed())
-//            )
-//
-//    }
+    @Test
+    fun scrollToPosition(){
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9))
+
+
+        onView(ViewMatchers.withText(R.string.affirmation10))
+            .check(
+                ViewAssertions.matches(ViewMatchers.isDisplayed())
+            )
+
+    }
 
 }
