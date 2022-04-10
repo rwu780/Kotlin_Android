fa32590763d89ce88bbfcfd62c616f3a29abcc9b
Rui Wu <ruiwu@Ruis-MacBook-Air.local>
2022-04-10 14:26:08 -0400

    Update fix action bar

--- Android_Basic_In_Kotlin/Unit_3_Navigation/CupCake_Data_Binding/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ Android_Basic_In_Kotlin/Unit_3_Navigation/CupCake_Data_Binding/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -20,7 +20,7 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-//import androidx.navigation.ui.setupActionBarWithNavController
+
 
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -29,22 +29,25 @@ import androidx.navigation.ui.setupActionBarWithNavController
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
 
-        
+
         setupActionBarWithNavController(navController)
+
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
+    override fun onNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp()
-                || super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onNavigateUp()
     }
+
 
 }
