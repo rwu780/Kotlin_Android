ae471b1c6af172e46f92ffbccc3a2e66c564889d
rwu780 <rwu4@ualberta.ca>
2022-04-30 16:26:51 -0400

    Add Hilt

---
+++ Android_Advanced/Dependency_Injection/android-hilt-main/app/src/main/java/com/example/android/hilt/data/LoggerDataSource.kt
@@ -0,0 +1,11 @@
+package com.example.android.hilt.data
+
+
+interface LoggerDataSource {
+
+    fun addLog(msg: String)
+    fun getAllLogs(callback: (List<Log>) -> Unit)
+    fun removeLogs()
+
+
+}
